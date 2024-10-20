d9075c31b6e59d6233af984e4fe7388ca657d195
Seresun <slavnyunstoppable22@gmail.com>
2024-10-20 14:32:04 +0200

    fix

--- src/components/MovieCast/MovieCast.jsx
+++ src/components/MovieCast/MovieCast.jsx
@@ -1,6 +1,6 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
-import { fetchMovieCast } from "../../services/api";
+import { fetchMovieCast } from "../../services/tmdbApi";
 
 function MovieCast() {
   const { movieId } = useParams();
